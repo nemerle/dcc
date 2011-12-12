9b9df8be6ebd985901b321077eccc96c47344648
Artur K <nemerle5@gmail.com>
2011-12-12 22:03:57 +0100

    before switch symtab from * to vector

--- src/chklib.cpp
+++ src/chklib.cpp
@@ -516,7 +516,6 @@ boolT LibCheck(Function & pProc)
             pProc.flg |= PROC_RUNTIME;		/* => is a runtime routine */
         }
     }
-
     if (locatePattern(prog.Image, pProc.procEntry,
                       pProc.procEntry+sizeof(pattMsChkstk),
                       pattMsChkstk, sizeof(pattMsChkstk), &Idx))
@@ -600,11 +599,12 @@ static boolT locatePattern(byte *source, Int iMin, Int iMax, byte *pattern, Int 
         for (j=0; j < iPatLen; j++)
         {
             /* j is the index of the byte being considered in the pattern. */
-            if ((*pSrc++ != pattern[j]) && (pattern[j] != WILD))
+            if ((*pSrc != pattern[j]) && (pattern[j] != WILD))
             {
                 /* A definite mismatch */
                 break;                      /* Break to outer loop */
             }
+            pSrc++;
         }
         if (j >= iPatLen)
         {
@@ -694,7 +694,7 @@ void STATE::checkStartup()
         but decides the model required. Note: must do the far data models
         (large and compact) before the others, since they are the same pattern
         as near data, just more pushes at the start. */
-    if(prog.cbImage>0x180)
+    if(prog.cbImage>0x180+sizeof(pattMainLarge))
     {
         if (locatePattern(prog.Image, startOff, startOff+0x180, pattMainLarge,sizeof(pattMainLarge), &i))
         {
