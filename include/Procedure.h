c0e9ba2fb35181b88aff646345529dfc882826b7
nemerle <nemerle5@gmail.com>
2016-04-25 10:03:30 +0200

    Add addOutEdgesForConditionalJump to header

--- include/Procedure.h
+++ include/Procedure.h
@@ -233,6 +233,8 @@ protected:
     void    genLiveKtes();
     bool    findDerivedSeq(derSeq &derivedGi);
     bool    nextOrderGraph(derSeq &derivedGi);
+    void addOutEdgesForConditionalJump(BB*        pBB, int next_ip, LLInst *ll);
+    
 private:
     bool    decodeIndirectJMP(ICODE &pIcode, STATE *pstate, CALL_GRAPH *pcallGraph);
     bool    decodeIndirectJMP2(ICODE &pIcode, STATE *pstate, CALL_GRAPH *pcallGraph);
